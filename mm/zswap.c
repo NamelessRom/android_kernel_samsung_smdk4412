0546320745ae974aa21568f083ed6000774525b8
Markus Elfring <elfring@users.sourceforge.net>
2014-12-12 16:57:18 -0800

    mm/zswap: delete unnecessary check before calling free_percpu()
    free_percpu() tests whether its argument is NULL and then returns
    immediately.  Thus the test around the call is not needed.
    
    This issue was detected by using the Coccinelle software.
    
    Change-Id: Iad1d706a3bfe9ba18c803e1406019952ce5c5f1f
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Cc: Seth Jennings <sjennings@variantweb.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/zswap.c
+++ mm/zswap.c
@@ -146,9 +146,8 @@ static int __init zswap_comp_init(void)
 
 static void __init zswap_comp_exit(void)
 {
-        /* free percpu transforms */
-        if (zswap_comp_pcpu_tfms)
-                free_percpu(zswap_comp_pcpu_tfms);
+	/* free percpu transforms */
+	free_percpu(zswap_comp_pcpu_tfms);
 }
 
 /*********************************
