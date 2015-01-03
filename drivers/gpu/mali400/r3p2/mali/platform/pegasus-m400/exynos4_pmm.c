5a1fec2a54f00a0a0670fefbd1fc0261fac37e8f
DerTeufel <dominik-kassel@gmx.de>
2015-01-04 00:33:32 +0100

    gpu/mali: remove use of atomic integer 'clk_active' in exynos4_pmm.c (from i9305 source drop
    Change-Id: I90874edb057e379e326c207ef605c62e77394d76

--- drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
+++ drivers/gpu/mali400/r3p2/mali/platform/pegasus-m400/exynos4_pmm.c
@@ -195,7 +195,6 @@ static unsigned int GPU_MHZ	= 1000000;
 
 int  gpu_power_state;
 static int bPoweroff;
-atomic_t clk_active;
 
 #define MAX_MALI_DVFS_STEPS 5
 static _mali_osk_atomic_t bottomlock_status;
@@ -450,7 +449,7 @@ void mali_clk_set_rate(unsigned int clk, unsigned int mhz)
 	MALI_DEBUG_PRINT(3, ("Mali platform: Setting frequency to %d mhz\n", clk));
 
 	if (mali_clk_get() == MALI_FALSE) {
-  	_mali_osk_lock_signal(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
+		_mali_osk_lock_signal(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
  		return;
 	}
 	
@@ -472,12 +471,9 @@ void mali_clk_set_rate(unsigned int clk, unsigned int mhz)
 		clk_set_parent(mali_clock, mali_parent_clock);
 	}
 
-	if (atomic_read(&clk_active) == 0) {
-		if (clk_enable(mali_clock) < 0) {
-			_mali_osk_lock_signal(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
- 			return;
-		}
-		atomic_set(&clk_active, 1);
+    if (gpu_power_state == 0) {
+		clk_enable(mali_clock);
+		gpu_power_state = 1;
 	}
 
 	err = clk_set_rate(mali_clock, rate);
@@ -491,7 +487,6 @@ void mali_clk_set_rate(unsigned int clk, unsigned int mhz)
 	mali_gpu_clk = (int)(rate / mhz);
 
 	mali_clk_put(MALI_FALSE);
-
 	_mali_osk_lock_signal(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
 }
 
@@ -869,7 +864,6 @@ mali_bool mali_dvfs_handler(unsigned int utilization)
 static mali_bool init_mali_clock(void)
 {
 	mali_bool ret = MALI_TRUE;
-	gpu_power_state = 1;
 	bPoweroff = 1;
 
 	if (mali_clock != 0)
@@ -957,13 +951,12 @@ static _mali_osk_errcode_t enable_mali_clocks(void)
 {
 	int err;
 
-	if (atomic_read(&clk_active) == 0) {
-		err = clk_enable(mali_clock);
-		MALI_DEBUG_PRINT(3,("enable_mali_clocks mali_clock %p error %d \n", mali_clock, err));
-		atomic_set(&clk_active, 1);
-		gpu_power_state = 1;
-	}
 
+    _mali_osk_lock_wait(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
+	clk_enable(mali_clock);
+	gpu_power_state = 1;
+    _mali_osk_lock_signal(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
+	
 	/* set clock rate */
 #ifdef CONFIG_MALI_DVFS
 	if (get_mali_dvfs_control_status() != 0 || mali_gpu_clk >= mali_runtime_resume.clk) {
@@ -989,17 +982,16 @@ static _mali_osk_errcode_t enable_mali_clocks(void)
 	maliDvfsStatus.currentStep = MALI_DVFS_DEFAULT_STEP;
 #endif
 
-
 	MALI_SUCCESS;
 }
 
 static _mali_osk_errcode_t disable_mali_clocks(void)
 {
-	if (atomic_read(&clk_active) == 1) {
-		clk_disable(mali_clock);
-		atomic_set(&clk_active, 0);
-		gpu_power_state = 0;
-	}
+    _mali_osk_lock_wait(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
+	clk_disable(mali_clock);
+	gpu_power_state = 0;
+    _mali_osk_lock_signal(mali_dvfs_lock, _MALI_OSK_LOCKMODE_RW);
+	
 	MALI_DEBUG_PRINT(3, ("disable_mali_clocks mali_clock %p \n", mali_clock));
 
 	MALI_SUCCESS;
@@ -1061,10 +1053,7 @@ _mali_osk_errcode_t mali_platform_init(struct device *dev)
 #ifdef EXYNOS4_ASV_ENABLED
 	mali_dvfs_table_update();
 #endif
-
 	MALI_CHECK(init_mali_clock(), _MALI_OSK_ERR_FAULT);
-
-	atomic_set(&clk_active, 0);
 
 #ifdef CONFIG_MALI_DVFS
 	/* Create sysfs for time-in-state */
